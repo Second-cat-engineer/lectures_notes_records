2773ccd0718272d047e96371d958c953a2065654
YaylyaevSafuan <yaylyaevsafuan@gmail.com>
2022-11-05 22:52:46 +0300

    Начало примера корзины интернет магазина

---
+++ oop-week/lesson04/example01/demo02/CartController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace lesson04\example01\demo02;
+
+class CartController
+{
+    public function actionIndex()
+    {
+        $items = Yii::$app->cart->getItems();
+
+        return $this->render('index', ['items' => $items]);
+    }
+
+    public function actionAdd($id, $count = 1)
+    {
+        $product = $this->findModel($id);
+
+        Yii::$app->cart->add($product->id, $count);
+
+        return $this->redirect(['index']);
+    }
+
+    public function actionRemove($id)
+    {
+        Yii::$app->cart->remove($id);
+
+        return $this->redirect(['index']);
+    }
+
+    private function findModel($id)
+    {
+        if (!$product = Product::findOne($id)) {
+            throw new NotFoundHttpException('Товар не найден.');
+        }
+        return $product;
+    }
+}
+
+class Product extends ActiveRecord
+{
+
+}
