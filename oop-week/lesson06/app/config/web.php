79e308c2ef8093ff6469bec20a13b5c3eb587a0f
Yaylyaev Safuan <yaylyaevsafuan@gmail.com>
2022-11-16 07:56:09 +0300

    Добавлены секреты

--- oop-week/lesson06/app/config/web.php
+++ oop-week/lesson06/app/config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => '06PDmMkEOuUZGZHV7zSLbFB1SXun4_1X',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
