62e8b1a35485c26b64e2445986d5596922a91f1f
Yaylyaev Safuan <yaylyaevsafuan@gmail.com>
2022-11-16 07:56:37 +0300

    Добавлены секреты

--- oop-week/lesson06/app/config/db.php
+++ oop-week/lesson06/app/config/db.php
@@ -1,11 +1,16 @@
 <?php
 
+use KebaCorp\VaultSecret\VaultSecret;
+
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+    'class'    => yii\db\Connection::class,
+    'dsn'      => 'mysql:host='
+        . VaultSecret::getSecret('MYSQL_DB_HOST') . ';port='
+        . VaultSecret::getSecret('MYSQL_DB_PORT') . ';dbname='
+        . VaultSecret::getSecret('MYSQL_DATABASE'),
+    'username' => VaultSecret::getSecret('MYSQL_USERNAME'),
+    'password' => VaultSecret::getSecret('MYSQL_PASSWORD'),
+    'charset'  => VaultSecret::getSecret('MYSQL_DATABASE_CHARSET'),
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
